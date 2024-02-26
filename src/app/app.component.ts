6b74580161a3612e34dc1311915b4102abc9c132
Nashit Humam <s_nashit@hotmail.com>
2024-02-26 16:55:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+
 })
 export class AppComponent {
   title = 'angular-crud-mongodb';
+
 }
